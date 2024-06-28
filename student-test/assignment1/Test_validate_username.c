013c93f32dc4cd7ab4d46020d7393be4b30d780d
Paul Kerchen <paul.kerchen@elektrobit.com>
2024-06-28 14:13:11 -0400

    Unity setup working

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,7 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    char*   username_from_file = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), username_from_file, "Hardcoded username doesn't match username read from file.");
+    free(username_from_file);
 }
