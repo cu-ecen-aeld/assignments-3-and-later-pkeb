a8237120afa33b6c500355e222ecfa81b9571f84
Paul Kerchen <paul.kerchen@elektrobit.com>
2024-07-01 14:20:18 -0400

    Implemented C file writer

---
+++ finder-app/writer.c
@@ -0,0 +1,37 @@
+//
+#include <stdio.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    openlog(NULL, LOG_PERROR, LOG_USER);
+
+    if (argc != 3) {
+        syslog(LOG_ERR, "Incorrect number of arguments\n");
+        syslog(LOG_ERR, "Usage: %s <filename> <contents>\n", argv[0]);
+        closelog();
+        return 1;
+    }
+    const char* filename = argv[1];
+    const char* contents = argv[2];
+
+    syslog(LOG_DEBUG, "Writing %s to %s", contents, filename);
+    int fd = creat(filename, S_IRUSR|S_IWUSR);
+    int    ret_code = 0;
+
+    if (fd < 0) {
+        syslog(LOG_ERR, "Could not open %s for writing\n", filename);
+        ret_code = 1;
+    } else if (write(fd, contents, strlen(contents)) != strlen(contents)) {
+        syslog(LOG_ERR, "Write to %s failed\n", filename);
+        ret_code = 1;
+    }
+
+    if (fd >= 0)
+        close(fd);
+    closelog();
+    return ret_code;
+}
