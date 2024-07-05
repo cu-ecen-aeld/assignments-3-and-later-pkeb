ffd0075c3dd2efccb5063936f349fad1ea215ac9
Paul Kerchen <paul.kerchen@elektrobit.com>
2024-07-05 15:37:13 -0400

    Don't build modules

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -37,7 +37,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
     make -j8 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    #make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 fi
 
